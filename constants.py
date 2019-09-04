325a62cea87278346577b6c685f056a19f79a076
Flavien <flavien.murail@gmail.com>
2019-09-04 10:12:08 +0200

    Rajout des éléments du jeu

---
+++ constants.py
@@ -0,0 +1,7 @@
+#windows size
+#images host links
+#standalone program
+#game ending
+    #when MC stands in front of the boss
+        #if MC collects every item == Win
+        #if MC misses one or several items == Lose
---
+++ main_program.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Program launching with Pygame windows (width = lenght)
+windows = pygame.display.set_mode((600, 600))
+
+#Background launching
+
+
+
+#VARIABLE
+
+#Structure
+    #one level
+    #game windows is a 15 sprites square
+    #labyrinth lenght == 15 squares
+
+
+
+
+

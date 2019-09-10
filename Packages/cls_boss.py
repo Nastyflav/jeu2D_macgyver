b9abb8f30329318ab236d2f5cba49ed2abce9499
Flavien <flavien.murail@gmail.com>
2019-09-10 11:36:20 +0200

    Map : méthode qui ajoute MG et le boss Macgyver : méthode qui permet au personnage de bouger Position : Ajout du pseudo-code pour expliquer l'algorithme Boss : Ajout du pseudo-code pour expliquer l'algorithme
    Avancée intéressant dans le chargement de la map, reste la problématique des items

--- Packages/cls_boss.py
+++ Packages/cls_boss.py
@@ -1,8 +1,11 @@
 class Boss:
 
     #character initialization
-    def __init__(self):
-        #character position
+    def __init__ (self, map):
+        #base map
+        self.map = map
+        #character position / using property from map.py, exit as an attribut
+        self.position = self.map.exit
 
         
     
--- Packages/cls_map.py
+++ Packages/cls_map.py
@@ -1,6 +1,7 @@
-from constants as constants
+from constants import
 from position import Position
 
+'''a Map class '''
 class Map:
 
     #Map creation
@@ -12,6 +13,14 @@ class Map:
         self._exit = set() 
         #Paths
         self._paths = set()
+        #Hero
+        self.hero = None
+        #Boss
+        self.boss = None
+        #Items
+        #Maze dimensions
+        self.height = None
+        self.width = None
         #File attribut
         self.load_from_file()
 
@@ -19,34 +28,47 @@ class Map:
     def start(self):
         return list(self.start)[0]
 
+    #is a position an available path ?
     def available_path(self, position):
         return position in self.paths
 
     #Loading map
     def load_from_file(self):
-        #Level scanning
+        '''Load the file (filename) content into the paths attribut. Identify as well the start and exit positions'''
         with open(self.filename) as infile:
             #Scanning loop
             for x, line in enumerate(infile):
                 for y, col in enumerate(line):
-                    #if there is a path square
+                    #if there is a path square (0 as PATH_CHAR)
                     if col == constants.PATHS_CHAR:
                         self._paths.add(Position, (x, y))
-                    #if this is the start square    
+                    #if this is the start square(S as START_CHAR)   
                     elif col == constants.START_CHAR:
                         self._start.add(Position(x, y))
                         self._paths.add(Position(x, y))
-                    #if this is the exit square       
+                    #if this is the exit square (E as EXIT_CHAR)       
                     elif col == constants.EXIT_CHAR:
                         self._exit.add(Position(x, y))
                         self._paths.add(Position(x, y))
+            #
+            self.height = x + 1
+            self.width = y + 1
 
-def main():
-    map = Map('level.txt')
+    def add_macgyver (self, macgyver)
+    '''Position MacGyver in the labyrinth'''
+        self.macgyver = macgyver
+        #MacGyver start position
+        self.macgyver.position = self.start
+        #So that MG can communicate with the Map class
+        self.macgyver.Map = self
 
-if __name__ == "__main__"":
-    main()
-    
+    def add_boss (self, boss)
+    '''Position MacGyver in the labyrinth'''
+        self.boss = boss
+        #Boss start position
+        self.boss.position = self.exit
+        #So that the boss can communicate with the Map class
+        self.boss.Map = self
 
 
 

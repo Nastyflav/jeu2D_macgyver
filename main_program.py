3d04f5e9fe662a6387195d0b5421b5a81963de77
Flavien <flavien.murail@gmail.com>
2019-09-16 16:06:48 +0200

    All the __pycache files have been purged

--- main_program.py
+++ main_program.py
@@ -1,11 +1,16 @@
-from Packages.Map_cls import Map
-from Packages.Position_cls import Position
+import os
+
+from Packages.Map import Map
+from Packages.Position import Position
+#from Packages.Macgyver import MacGyver
+#from Packages.Boss import Boss
 from Settings.constants import *
 
 
 def main():
     map = Map('Maps/level.txt')
-    print(map._paths)
+    print(map._exit)
+    
     input("Choose your next move")
     
 if __name__ =="__main__":

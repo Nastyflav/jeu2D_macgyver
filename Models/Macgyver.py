2e4da55efae0131c740959a77e4f94a5984bd0ec
Flavien <flavien.murail@gmail.com>
2019-10-31 11:45:29 +0100

    Revert "New organisation in the repository"
    This reverts commit 4faff20983bb301d6f0aa1ea4384e47eacfa1736.

---
+++ Models/Macgyver.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+# coding: utf-8
+"""The hero class, managing movements and actions"""
+
+
+class Macgyver:
+    """Class to manage the Macgyver moves and the items picking into the maze"""
+    def __init__(self, map):
+        """Start position and items counter"""
+        self.x = 0
+        self.y = 12
+        self.map = map
+        self.items_collected = 0
+
+    def move_up(self, map):
+        """Check if the hero can move up and if he's on an item square"""
+        if map.map_array[self.y - 1][self.x] != 'W':
+            map.map_array[self.y][self.x] = '.'
+            self.y -= 1
+            map.map_array[self.y][self.x] = 'M'
+            self.collect_items(map)
+        return map
+
+    def move_down(self, map):
+        """Check if the hero can move down and if he's on an item square"""
+        if map.map_array[self.y + 1][self.x] != 'W':
+            map.map_array[self.y][self.x] = '.'
+            self.y += 1
+            map.map_array[self.y][self.x] = 'M'
+            self.collect_items(map)
+        return map
+
+    def move_right(self, map):
+        """Check if the hero can move right and if he's on an item square"""
+        if map.map_array[self.y][self.x + 1] != 'W':
+            map.map_array[self.y][self.x] = '.'
+            self.x += 1
+            map.map_array[self.y][self.x] = 'M'
+            self.collect_items(map)
+        return map
+
+    def move_left(self, map):
+        """Check if the hero can move left and if he's on an item square"""
+        if map.map_array[self.y][self.x - 1] != 'W':
+            map.map_array[self.y][self.x] = '.'
+            self.x -= 1
+            map.map_array[self.y][self.x] = 'M'
+            self.collect_items(map)
+        return map
+
+    def collect_items(self, map):
+        '''Check if the hero position is also an item position, and increments the backpack
+        The item position is moved away to avoid repetitions'''
+        if (self.y, self.x) == map.items[0]:
+            self.items_collected += 1
+            map.items[0] = (14, 0)
+        elif (self.y, self.x) == map.items[1]:
+            self.items_collected += 1
+            map.items[1] = (14, 1)
+        elif (self.y, self.x) == map.items[2]:
+            self.items_collected += 1
+            map.items[2] = (14, 2)
